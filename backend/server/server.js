3a4b77e027cf079996ec9ba1cad462980ca3708d
ASUS <ASUS@LAPTOP-UNDN5F42>
2021-06-18 11:59:54 +0530

    client folder made

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,5 +1,14 @@
 const app = require('../app');
 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.listen(process.env.PORT || 4000, () => console.log("server is up and running"));
 
 module.exports = app;
