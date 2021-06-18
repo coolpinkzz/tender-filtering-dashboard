25e098c2cf0efbd2714c84d69d296ccd548e8ed1
ASUS <ASUS@LAPTOP-UNDN5F42>
2021-06-18 13:49:33 +0530

    relative url added

--- backend/server/controllers/userControllers.js
+++ backend/server/controllers/userControllers.js
@@ -2,281 +2,277 @@ const User = require('../../models/signupmodels');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 const validateEmail = require("../../validation/validateEmail");
-const validateRole =  require("../../validation/validateRole");
-const validateName =  require("../../validation/validateName");
+const validateRole = require("../../validation/validateRole");
+const validateName = require("../../validation/validateName");
 const validatePasswordInput = require("../../validation/validatePassword");
-const { roles } = require('../roles')
+const {roles} = require('../roles')
 
-exports.grantAccess = function(action, resource) {
+exports.grantAccess = function (action, resource) {
 
- return async (req, res, next) => {
-  try {
-   email = req.query.email;
-   //res.status(200).json(email);
-  const user = await User.findOne({email});
-  if(user ) {
-    const role = user.role;
-    //res.status(200).json({id: role});
-    const permission = roles.can(role)[action](resource);
-     //res.status(400).json({message:permission.granted});
-     if (permission.granted == false) {
-       return res.status(401).json({
-         error: "You don't have enough permission to perform this action"
-       });
-     }
-    next()
-  }
-  else{
-    return res.status(401).json({
-      error: "You don't have enough permission to perform this action"
-    });
-  }
-  }
-  catch (error) {
-    next(error)
-  }
-}
+    return async (req, res, next) => {
+        try {
+            email = req.query.email;
+            //res.status(200).json(email);
+            const user = await User.findOne({email});
+            if (user) {
+                const role = user.role;
+                //res.status(200).json({id: role});
+                const permission = roles.can(role)[action](resource);
+                //res.status(400).json({message:permission.granted});
+                if (permission.granted == false) {
+                    return res.status(401).json({
+                        error: "You don't have enough permission to perform this action"
+                    });
+                }
+                next()
+            }
+            else {
+                return res.status(401).json({
+                    error: "You don't have enough permission to perform this action"
+                });
+            }
+        }
+        catch (error) {
+            next(error)
+        }
+    }
 }
 
 exports.allowIfLoggedin = async (req, res, next) => {
- try {
+    try {
 
-  const user = req.query.auth;
+        const user = req.query.auth;
 
-  if (typeof user === 'string' || user instanceof String)
-    userBool = (user.toLowerCase() === "true");
-  else {
-    userBool = user;
-  }
-  //res.status(400).json({user:user});
-  if (userBool==false) {
-   return res.status(400).json({
-    error: "You need to be logged in to access this route"
-    });
-  }
-   next();
-  } catch (error) {
-   next(error);
-  }
+        if (typeof user === 'string' || user instanceof String)
+            userBool = (user.toLowerCase() === "true");
+        else {
+            userBool = user;
+        }
+        //res.status(400).json({user:user});
+        if (userBool == false) {
+            return res.status(400).json({
+                error: "You need to be logged in to access this route"
+            });
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
 }
 
 async function hashPassword(password) {
- return await bcrypt.hash(password, 10);
+    return await bcrypt.hash(password, 10);
 }
 
 async function validatePassword(plainPassword, hashedPassword) {
- return await bcrypt.compare(plainPassword, hashedPassword);
+    return await bcrypt.compare(plainPassword, hashedPassword);
 }
 
 exports.signup = async (req, res, next) => {
- try {
-   // Validation code here
-   const { errors, isValid } = validateRegisterInput(req.body);
-   // Check validation
-   if (!isValid) {
-     return res.status(400).json(errors);
-   }
+    try {
+        // Validation code here
+        const {errors, isValid} = validateRegisterInput(req.body);
+        // Check validation
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
 
-  const hashedPassword = await hashPassword(req.body.password);
-  const email = req.body.email;
-  const user = await User.findOne({ email });
-  if (user) return res.status(400).json({data: "User already exists."});
+        const hashedPassword = await hashPassword(req.body.password);
+        const email = req.body.email;
+        const user = await User.findOne({email});
+        if (user) return res.status(400).json({data: "User already exists."});
 
-  const signedupUser = new User({
-   name: req.body.name,
-   email: req.body.email,
-   password: hashedPassword,
-   password2: hashedPassword,
-   role: req.body.role || "staff-member",
-   createdById: req.body.createdBy,
-   createdByName: req.body.createdByName
-  });
+        const signedupUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+            password2: hashedPassword,
+            role: req.body.role || "staff-member",
+            createdById: req.body.createdBy,
+            createdByName: req.body.createdByName
+        });
 
-  await signedupUser.save()
-  res.status(200).json({message:"User created."})
+        await signedupUser.save()
+        res.status(200).json({message: "User created."})
+    }
+    catch (error) {
+        next(error)
+    }
 }
-catch(error) {
-   next(error)
- }
-}
 
-exports.login = async(req, res, next) => {
-  try {
-    //validation code here
-    const { errors, isValid } = validateLoginInput(req.body);
-    // Check validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+exports.login = async (req, res, next) => {
+    try {
+        //validation code here
+        const {errors, isValid} = validateLoginInput(req.body);
+        // Check validation
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
 
-    const email = req.body.email;
-    const password = req.body.password;
+        const email = req.body.email;
+        const password = req.body.password;
 
-    const user = await User.findOne({ email });
-    if (!user) {
-      errors.email = 'Email does not exist.';
-      return res.status(400).json(errors);
-    }
+        const user = await User.findOne({email});
+        if (!user) {
+            errors.email = 'Email does not exist.';
+            return res.status(400).json(errors);
+        }
 
-    const validPassword = await validatePassword(password, user.password);
-    if (!validPassword) {
-      errors.password = 'Password is incorrect.';
-      return res.status(400).json(errors);
-    }
+        const validPassword = await validatePassword(password, user.password);
+        if (!validPassword) {
+            errors.password = 'Password is incorrect.';
+            return res.status(400).json(errors);
+        }
 
-    const payload = {
-          id: user._id,
-          name: user.name,
-          role: user.role,
-          email: user.email,
-          tableData: {}
-    };
+        const payload = {
+            id: user._id,
+            name: user.name,
+            role: user.role,
+            email: user.email,
+            tableData: {}
+        };
 
-    const token = jwt.sign(
-          payload,
-          process.env.JWT_SECRET,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          }
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {
+                expiresIn: 31556926 // 1 year in seconds
+            }
         );
 
-    res.status(200).json({
-      success: true,
-      token: "Bearer " + token
-    });
-  }
-  catch(error) {
-      next(error);
-  }
+        res.status(200).json({
+            success: true,
+            token: "Bearer " + token
+        });
+    }
+    catch (error) {
+        next(error);
+    }
 }
 
 exports.getUsers = async (req, res, next) => {
-  const users = await User.find({});
-  res.status(200).json({
-    data: users
-  });
+    const users = await User.find({});
+    res.status(200).json({
+        data: users
+    });
 }
 
 exports.getUser = async (req, res, next) => {
-  try {
-    const email = req.query.email;
-  //  res.status(200).json(({email}));
-    const user = await User.findOne({email});
+    try {
+        const email = req.query.email;
+        //  res.status(200).json(({email}));
+        const user = await User.findOne({email});
 
-    if (!user) return next(new Error('User does not exist.'));
-    res.status(200).json({
-      data: user
-    });
-  }
-  catch (error) {
-    next(error)
-  }
-}
- //validate role
-exports.updateUser = async (req, res, next) => {
- try {
-   const email = req.query.email;
-   const email_upd = req.query.emailupdate;
-
-   const user_req = await User.find({email:email});
-   const user_upd = await User.find({email:email_upd});
-
-   const userRole = user_req[0].role;
-   const updateRole = user_upd[0].role;
-
-   if (userRole == "admin") {
-     if(updateRole == "super-admin" || updateRole == "admin") {
-       res.status(401).json({
-         error: "Action forbidden: Not allowed to change this user."
-       });
-      }
+        if (!user) return next(new Error('User does not exist.'));
+        res.status(200).json({
+            data: user
+        });
     }
+    catch (error) {
+        next(error)
+    }
+}
+//validate role
+exports.updateUser = async (req, res, next) => {
+    try {
+        const email = req.query.email;
+        const email_upd = req.query.emailupdate;
 
-   const userBody = req.body;
+        const user_req = await User.find({email: email});
+        const user_upd = await User.find({email: email_upd});
 
-   if (userBody.email)
-   {
-     const { errors, isValid } = validateEmail(userBody);
-     // Check validation
-     if (!isValid) {
-       return res.status(400).json(errors);
-     }
-   }
+        const userRole = user_req[0].role;
+        const updateRole = user_upd[0].role;
 
-   if (userBody.role)
-   {
-     const { errors, isValid } = validateRole(userBody);
-     // Check validation
-     if (!isValid) {
-       return res.status(400).json(errors);
-     }
-   }
+        if (userRole == "admin") {
+            if (updateRole == "super-admin" || updateRole == "admin") {
+                res.status(401).json({
+                    error: "Action forbidden: Not allowed to change this user."
+                });
+            }
+        }
 
-   if (userBody.name)
-   {
-     const { errors, isValid } = validateName(userBody);
-     // Check validation
-     if (!isValid) {
-       return res.status(400).json(errors);
-     }
-   }
+        const userBody = req.body;
 
-   if (userBody.password)
-   {
-     const { errors, isValid } = validatePasswordInput(userBody);
-     // Check validation
-     if (!isValid) {
-       return res.status(400).json(errors);
-     }
-     userBody.password = await hashPassword(userBody.password);
-     userBody.password2 = userBody.password;
-   }
+        if (userBody.email) {
+            const {errors, isValid} = validateEmail(userBody);
+            // Check validation
+            if (!isValid) {
+                return res.status(400).json(errors);
+            }
+        }
 
-   await User.findByIdAndUpdate(user_upd[0]._id, userBody);
-   const user = await User.findById(user_upd[0]._id);
+        if (userBody.role) {
+            const {errors, isValid} = validateRole(userBody);
+            // Check validation
+            if (!isValid) {
+                return res.status(400).json(errors);
+            }
+        }
 
-   res.status(200).json({
-    data: user,
-    message: 'User updated successfully.'
-   });
-  }
-  catch (error) {
-   next(error)
-  }
+        if (userBody.name) {
+            const {errors, isValid} = validateName(userBody);
+            // Check validation
+            if (!isValid) {
+                return res.status(400).json(errors);
+            }
+        }
+
+        if (userBody.password) {
+            const {errors, isValid} = validatePasswordInput(userBody);
+            // Check validation
+            if (!isValid) {
+                return res.status(400).json(errors);
+            }
+            userBody.password = await hashPassword(userBody.password);
+            userBody.password2 = userBody.password;
+        }
+
+        await User.findByIdAndUpdate(user_upd[0]._id, userBody);
+        const user = await User.findById(user_upd[0]._id);
+
+        res.status(200).json({
+            data: user,
+            message: 'User updated successfully.'
+        });
+    }
+    catch (error) {
+        next(error)
+    }
 }
 
 exports.deleteUser = async (req, res, next) => {
- try {
+    try {
 
-  const email = req.query.email;
-  const email_del = req.query.emailDelete;
+        const email = req.query.email;
+        const email_del = req.query.emailDelete;
 
-  const user_req = await User.find({email:email});
-  const user_delete = await User.find({email:email_del});
+        const user_req = await User.find({email: email});
+        const user_delete = await User.find({email: email_del});
 
-  const userRole = user_req[0].role;
-  const deleteRole = user_delete[0].role;
+        const userRole = user_req[0].role;
+        const deleteRole = user_delete[0].role;
 
-  if (userRole == "admin" && (deleteRole == "super-admin" || deleteRole == "admin")) {
-    res.status(401).json({
-      error: "Action forbidden: Not allowed to change this user."
-    });
-  }
-  else{
+        if (userRole == "admin" && (deleteRole == "super-admin" || deleteRole == "admin")) {
+            res.status(401).json({
+                error: "Action forbidden: Not allowed to change this user."
+            });
+        }
+        else {
 
-    await User.findByIdAndDelete(user_delete[0]._id);
-    res.status(200).json({
-     data: null,
-     message: 'User has been deleted'
-    });
-  }
+            await User.findByIdAndDelete(user_delete[0]._id);
+            res.status(200).json({
+                data: null,
+                message: 'User has been deleted'
+            });
+        }
 
- }
- catch (error) {
-  next(error)
- }
+    }
+    catch (error) {
+        next(error)
+    }
 }
--- backend/server/server.js
+++ backend/server/server.js
@@ -2,14 +2,14 @@ const app = require('../app');
 var express = require('express');
 
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
 
-}
+app.use(express.static("client/build"));
+const path = require("path");
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
+
+
 
 app.listen(process.env.PORT || 4000, () => console.log("server is up and running"));
 
